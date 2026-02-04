a1c37d43e25aa36d476eb5b9e8c9374d8b7d44b5
BhavishKumar03 <bhavishkumar1008.com>
2026-02-04 10:52:43 +0530

    Day3 Commit

--- src/Day3/day3_python_fundamentals.py
+++ src/Day3/day3_python_fundamentals.py
@@ -23,3 +23,27 @@ a.sort()
 print(a)
 a.reverse()
 print(a)
+
+# Task 1: The Inventory Manager Code
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+print("Current Inventory:", inventory)
+inventory.append("Eggs")
+inventory.remove("Bananas")
+inventory.sort()
+print("Final Updated Inventory:", inventory)
+
+# Task 2: The Data Slicer Code  
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print("First Reading:", temperatures[0])
+print("Last Reading:", temperatures[-1])
+afternoon_peak = temperatures[3:6]
+print("Afternoon Peak Readings:", afternoon_peak)
+last_three_hours = temperatures[-3:]
+print("Last 3 Hours Readings:", last_three_hours)
+
+# Task 3: The Immutable Config Code
+screen_res = (1920, 1080)
+print(f"Current Resolution: {screen_res[0]}x{screen_res[1]}")
+#screen_res[0] = 1280   #  TypeError: 'tuple' object does not support item assignment
+print("Tuples cannot be modified!")
+
