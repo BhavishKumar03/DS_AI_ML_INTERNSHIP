435f2758158dbffb0ea57ddf2b3bccb6730a5430
BhavishKumar03 <bhavishkumar1008.com>
2026-02-06 12:49:35 +0530

    Day5 Tasks Completed

---
+++ src/Day5/day5_functions_and_modules.py
@@ -0,0 +1,50 @@
+#simple function to greet
+def greet():
+    print("Hello, welcome to the Internet of Things course!")
+greet()
+
+#function to add two numbers
+def add_numbers(a, b):
+    return a + b
+result = add_numbers(5,3)
+print("The sum is:", result)
+
+#demonstrating variable scope
+x=10
+def show_value():
+    x=5
+    print(x)
+show_value()
+print(x)
+
+vegetable="carrot"
+def print_message():
+    fruit="apple"
+    animal="dog"
+    print("Vegetable:", vegetable)
+    print("Fruit:", fruit)
+print_message()
+#print("Animal:", animal) # This will cause an error because 'animal' is not defined in this scope
+
+import math
+import random
+print(math.sqrt(16))
+print(random.randint(1, 10))
+print(random.uniform(0, 1))
+print(random.choice(['apple', 'banana', 'cherry']))
+print(random.sample([1, 2, 3, 4, 5], 3))
+print(random.shuffle([1, 2, 3, 4, 5]))
+print(random.randrange(1, 10, 2))
+print(random.seed(42))
+print(random.random())
+
+#Example of NameError
+#print(b) # This will cause a NameError because 'b' is not defined   
+
+#hello()
+
+#example of import error
+#from math import squareroot
+
+#example of module not found error
+#import numpyyy
---
+++ src/Day5/myfunction,py
@@ -0,0 +1,4 @@
+import mymodule
+
+print(mymodule.greet("Bhavish"))
+print(mymodule.add(10, 20))
