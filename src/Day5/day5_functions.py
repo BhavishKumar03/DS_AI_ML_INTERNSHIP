6ac0e1f3a7c58890b372e6872718068f7a951ee5
BhavishKumar03 <bhavishkumar1008.com>
2026-02-06 14:25:17 +0530

    Day5 Tasks Completed with screenshots

--- src/Day5/day5_functions.py
+++ src/Day5/day5_functions.py
@@ -48,3 +48,14 @@ print(random.random())
 
 #example of module not found error
 #import numpyyy
+
+
+#Calculating area and perimeter of a rectangle
+def calc_rectangle(length, width):
+    area = length * width
+    perimeter = 2 * (length + width)
+    return area, perimeter  
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+area, perimeter = calc_rectangle(length, width)
+print(f"Area: {area}, Perimeter: {perimeter}")
--- src/Day5/main.py
+++ src/Day5/main.py
@@ -1,2 +1,7 @@
-import utils
-print(utils.multiply(3,4))
+import math_operations
+
+result_power = math_operations.power(2, 10)
+result_avg = math_operations.average([10, 20, 30, 40])
+
+print("2^10 =", result_power)
+print("Average =", result_avg)
