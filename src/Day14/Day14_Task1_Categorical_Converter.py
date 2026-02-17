d7c7790302ae4fb5eff7a8a89b8a4fa28fc58f96
BhavishKumar03 <bhavishkumar1008.com>
2026-02-17 14:12:34 +0530

    Day14 task is submitted

--- src/Day14/Day14_Task1_Categorical_Converter.py
+++ src/Day14/Day14_Task1_Categorical_Converter.py
@@ -1,16 +1,37 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-
-data = {
-    "Transmission": ["Automatic", "Manual", "Automatic", "Manual", "Automatic"],
-    "Color": ["Red", "Blue", "Green", "Red", "Blue"]
-}
-
-df = pd.DataFrame(data)
+data={"Transmission":["Automatic","Manual","Automatic","Manual","Automatic"],
+      "Color":["Red","Blue","Green","Red","Blue"]}
+df=pd.DataFrame(data)
 print("Original Data:")
 print(df)
-le = LabelEncoder()
-df["Transmission"] = le.fit_transform(df["Transmission"])
-df = pd.get_dummies(df, columns=["Color"], drop_first=True)
-print("Encoded Data:")
+print("Label Encoding used for: Transmission")
+le=LabelEncoder()
+df["Transmission"]=le.fit_transform(df["Transmission"])
+print("One-Hot Encoding used for: Color (drop_first=True applied)")
+df=pd.get_dummies(df,columns=["Color"],drop_first=True)
 print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
