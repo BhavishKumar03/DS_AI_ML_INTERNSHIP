6cd384082bf6c8c1e2a05c5205e8656c90f040ed
BhavishKumar03 <bhavishkumar1008.com>
2026-02-21 20:11:09 +0530

    Day18 SQL related tasks are completed

---
+++ src/Day18/day18_Task1_Insight_Filter.py
@@ -0,0 +1,32 @@
+import sqlite3
+import pandas as pd
+
+conn=sqlite3.connect("Day18_Internship.db")
+df=pd.read_sql_query("SELECT * FROM interns", conn)
+print(f"All data Retreived :\n{df}")
+
+gt5000=pd.read_sql_query("select * from interns where track='Data Science' and stipend>5000;",conn)
+print(f"\nData Science interns with Greater than 5000 stipend: \n{gt5000}")
+
+gp_track=pd.read_sql_query("select track, avg(stipend) as Average_Stipend from interns group by track",conn)
+print(f"\nAverage Stipend for each track: \n{gp_track}")
+
+cnt_track=pd.read_sql_query("SELECT track, COUNT(*) AS intern_count FROM interns GROUP BY track;",conn)
+print(f"\Count of Employees / Interns for each track: \n{cnt_track}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
